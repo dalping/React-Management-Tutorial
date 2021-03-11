a59b18de7aa3b30b6972574bc2c1b867b0a962f3
Dalping <74459925+Bokyung626@users.noreply.github.com>
2021-03-12 05:58:01 +0900

    add2

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
           customers.map(c=> {
             return(
               <Customer
+                key = {c.id}
                 id ={c.id}
                 image ={c.image}
                 name ={c.name}
